92650c6970af109c988ead2f3cc330f0f86d13e5
Magi <egorrak123@gmail.com>
2025-09-25 14:05:03 +0300

    pre-alpha 0.0.5

--- src/main/java/net/amagitsyne/ichorio/datagen/ModItemModelProvider.java
+++ src/main/java/net/amagitsyne/ichorio/datagen/ModItemModelProvider.java
@@ -24,7 +24,7 @@ public class ModItemModelProvider extends ItemModelProvider {
     basicItem(ModItems.NETHERINE.get());
 
         buttonItem(ModBlocks.ANCIENTWOOD_BUTTON, ModBlocks.ANCIENTWOOD_PLANKS);
-        fenceItem(ModBlocks.ANCIENTWOOD_BUTTON, ModBlocks.ANCIENTWOOD_PLANKS);
+        fenceItem(ModBlocks.ANCIENTWOOD_FENCE, ModBlocks.ANCIENTWOOD_PLANKS);
     }
     public void buttonItem(DeferredBlock<?> block, DeferredBlock<Block> baseBlock) {
         this.withExistingParent(block.getId().getPath(), mcLoc("block/button_inventory"))
